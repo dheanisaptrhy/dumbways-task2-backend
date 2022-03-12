65d01451f20eef133d0de18f5ce316bc4d54fcc9
dlystwist <kurogoblast@gmail.com>
2022-03-13 00:37:07 +0700

    add login

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,14 @@ const router = express.Router()
 
 // import controller
 const {addUser} = require('../controllers/user')
-const {register} = require('../controllers/auth')
+const {register, login} = require('../controllers/auth')
 
 //Router User
 router.post('/user',addUser)
 
 //router auth
 router.post('/register',register)
+router.post('/login',login)
 
 // export router
 module.exports = router
