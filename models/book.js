fdab94f5e0b5360e193791ff86ecf3f347394542
dlystwist <kurogoblast@gmail.com>
2022-03-13 17:11:47 +0700

    add book

--- models/book.js
+++ models/book.js
@@ -11,7 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      
+      book.belongsTo(models.user, {
+        as: 'user',
+        foreignKey:{
+          name:'idUser'
+        }
+      })
     }
   }
   book.init({
